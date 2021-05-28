0325920596ebf27b1cc96948925929dd0776158b
datdb-dev <datdb.94@gmail.com>
2021-05-28 18:17:00 +0700

    init code

---
+++ app/src/main/java/com/datdb/base/BaseActivity.java
@@ -0,0 +1,37 @@
+package com.datdb.base;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
+
+public abstract class BaseActivity<T extends ViewDataBinding> extends AppCompatActivity implements View.OnClickListener {
+    protected T binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this, getLayout());
+        init();
+        initBinding();
+        initListener();
+        binding.setLifecycleOwner(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        onClick(view.getId());
+    }
+
+    protected abstract int getLayout();
+
+    protected abstract void init();
+
+    protected abstract void initBinding();
+
+    protected abstract void initListener();
+
+    protected abstract void onClick(int id);
+}
---
+++ app/src/main/java/com/datdb/base/BaseFragment.java
@@ -0,0 +1,48 @@
+package com.datdb.base;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
+import androidx.fragment.app.Fragment;
+
+public abstract class BaseFragment<T extends ViewDataBinding> extends Fragment implements View.OnClickListener {
+
+    protected T binding;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, getLayout(), container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        init();
+        initBinding();
+        initListener();
+        binding.setLifecycleOwner(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        onClick(view.getId());
+    }
+
+    protected abstract int getLayout();
+
+    protected abstract void init();
+
+    protected abstract void initBinding();
+
+    protected abstract void initListener();
+
+    protected abstract void onClick(int id);
+}
